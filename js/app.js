11aa6707de401501291ac40a481cc9045f382721
redwan100 <ridoneislam987@gmail.com>
2021-06-26 12:40:23 +0600

    added subscribe section

--- js/app.js
+++ js/app.js
@@ -38,9 +38,45 @@ function scrollHeader(){
     const header = document.getElementById('header');
     if (this.scrollY >= 100) {
         header.classList.add('scroll-header');
-    } else {
+    } else { 
         header.classList.remove('scroll-header');
     }
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+
+// TODO: Video File=======
+
+const videoFile = document.getElementById('video_file');
+const videoButton = document.getElementById('video_button');
+const videoIcon = document.getElementById('video-icon');
+
+
+function playPause() {
+    if (videoFile.paused) {
+        // Play video
+        videoFile.play();
+        
+        // we change icon
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+
+    } else {
+        // video pause
+        videoFile.pause();
+ 
+        // we change the icon
+        videoFile.classList.remove('ri-pause-line');
+        videoFile.classList.add('ri-play-line')
+    }
+}
+ 
+videoButton.addEventListener('click', playPause);
+
+function finalVideo() {
+    videoIcon.classList.remove("ri-pause-line");
+    videoIcon.classList.add("ri-play-line");
+}
+
+videoFile.addEventListener('ended', finalVideo);
